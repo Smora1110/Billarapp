538a6c648d6f6e6f685af0892f15775cef319836
Sebastian <pviviescas@unab.edu.co>
2023-11-09 11:24:28 -0500

    commit pau6

--- finalizar.php
+++ finalizar.php
@@ -64,6 +64,7 @@ $nombre_usuario=$_SESSION['nombre_usuario'];
               </div>
               <div class="form-group">
                 <label>Mesa</label>
+                <label>Mesa</label>
                 <input value="<?php print $nombre_usuario; ?>" type="text" class="form-control" name="mesa"required>
               </div>
 
